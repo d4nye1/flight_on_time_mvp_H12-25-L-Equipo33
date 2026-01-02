7bca87a4a45c389ac327b9846c299b6273a38854
Ernesto López <91957919+d4nye1@users.noreply.github.com>
2026-01-01 23:39:09 -0600

    actualizadtoglobal

--- src/main/java/com/flightontime/flightontimeapi/controller/PredictController.java
+++ src/main/java/com/flightontime/flightontimeapi/controller/PredictController.java
@@ -8,10 +8,8 @@ import com.flightontime.flightontimeapi.dto.FlightRequestDTO;
 import com.flightontime.flightontimeapi.dto.FlightPredictionDTO;
 import com.flightontime.flightontimeapi.service.FlightPredictionService;
 
-//@RestController
-//@RequestMapping("/predict")
 @RestController
-@RequestMapping("/api/flights")
+@RequestMapping("/predict")
 public class PredictController {
 
     private final FlightPredictionService service;
@@ -20,8 +18,7 @@ public class PredictController {
         this.service = service;
     }
 
-    //@PostMapping
-    @PostMapping("/predict")
+    @PostMapping
     public FlightPredictionDTO predict(
             @Validated(ValidacionGrupos.SecuenciaOrdenada.class) @RequestBody FlightRequestDTO request) {
 
