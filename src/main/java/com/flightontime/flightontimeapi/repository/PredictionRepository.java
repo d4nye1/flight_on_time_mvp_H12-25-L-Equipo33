1dee6bfe2cbda0e37ace275437d398d2a9059282
Ernesto López <91957919+d4nye1@users.noreply.github.com>
2026-01-08 11:37:20 -0600

    agregar validacionde vuelo existente

--- src/main/java/com/flightontime/flightontimeapi/repository/PredictionRepository.java
+++ src/main/java/com/flightontime/flightontimeapi/repository/PredictionRepository.java
@@ -3,7 +3,22 @@ package com.flightontime.flightontimeapi.repository;
 import com.flightontime.flightontimeapi.entity.Prediction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 public interface PredictionRepository extends JpaRepository<Prediction, Long> {
 
-    boolean existsByEstado(String estado);
+    boolean existsByAerolineaAndOrigenAndDestinoAndFechaPartida(
+            String aerolinea,
+            String origen,
+            String destino,
+            LocalDateTime fechaPartida
+    );
+
+    Optional<Prediction> findByAerolineaAndOrigenAndDestinoAndFechaPartida(
+            String aerolinea,
+            String origen,
+            String destino,
+            LocalDateTime fechaPartida
+    );
 }
