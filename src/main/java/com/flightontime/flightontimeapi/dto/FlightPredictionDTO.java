333af201323e69bbca05fa7e163a004069a6a3ca
Ernesto López <91957919+d4nye1@users.noreply.github.com>
2026-01-08 12:03:59 -0600

    fechaDePartida

--- src/main/java/com/flightontime/flightontimeapi/dto/FlightPredictionDTO.java
+++ src/main/java/com/flightontime/flightontimeapi/dto/FlightPredictionDTO.java
@@ -10,7 +10,7 @@ public class FlightPredictionDTO {
     @JsonProperty("probabilidad")
     private Double probabilidad;
 
-    @JsonProperty("distancia") // ✅ Nuevo campo añadido
+    @JsonProperty("distancia_km")
     private Double distancia;
 
     public FlightPredictionDTO() {
